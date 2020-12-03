4e3aa80df8ffc5c9522ee39ebc6db0f436f2305c
TeamAI3 <teesproject.ai@gmail.com>
2020-12-03 20:24:32 +0000

    adds button to add task

--- app/src/main/java/com/example/to_do/MainActivity.java
+++ app/src/main/java/com/example/to_do/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.to_do;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void Task(View view){
+        Intent addtask= new Intent(MainActivity.this, AddTaskActivity.class);
+        startActivity(addtask);
+    }
+
 }

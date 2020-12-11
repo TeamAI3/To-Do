97c42ead8e55bf24241035edc959d144fe7c61a5
TeamAI3 <teesproject.ai@gmail.com>
2020-12-11 15:44:51 +0000

    added Voice commands to perform Task

--- app/src/main/java/com/example/to_do/MainActivity.java
+++ app/src/main/java/com/example/to_do/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
+    private TextToSpeech tts;
     final String userUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Data").child(userUid);
     private Adapter adapter;
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        initializeTextToSpeech();
 
         list = new ArrayList<>();
         adapter = new Adapter(this, list);
@@ -56,9 +61,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initializeTextToSpeech() {
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (tts.getEngines().size() == 0) {
+                    Toast.makeText(MainActivity.this, "Engine is not available", Toast.LENGTH_SHORT).show();
+                } else {
+                    assistant("Click on the + button to add new task");
+                }
+            }
+        });
+    }
+
+    private void assistant(String msg) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(msg, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            tts.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
     public void Task(View view) {
         Intent addtask = new Intent(MainActivity.this, AddTaskActivity.class);
         startActivity(addtask);
     }
-
 }
